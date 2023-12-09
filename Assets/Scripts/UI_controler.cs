c38286fd6498052dba53030069a8adb4412faada
sopipc167 <sopipc167@gmail.com>
2023-12-09 17:23:30 +0900

    버튼마다 URL첨부

--- Assets/Scripts/UI_controler.cs
+++ Assets/Scripts/UI_controler.cs
@@ -23,12 +23,36 @@ public class UI_controler : MonoBehaviour
         parser = JSON_PARSER.instance;
         post_list data = parser.readJSON(F_NAME);
     }*/
-    public void OnButtonClick()
+    public void univ()
     {
         Application.OpenURL("https://www.all-con.co.kr/view/contest/462840");
     }
     public void Sejong()
     {
         Application.OpenURL("http://www.sejong.ac.kr/");
+    }
+    public void ssafy()
+    {
+        Application.OpenURL("https://www.ssafy.com/ksp/servlet/swp.content.controller.SwpContentServlet?p_process=select-content-view&p_menu_cd=M0306&p_content_cd=C0306");
+    }
+    public void Tosc()
+    {
+        Application.OpenURL("http://www.tosc.kr/");
+    }
+    public void adogen()
+    {
+        Application.OpenURL("https://m.facebook.com/sejongIME/posts/2308840419385593/?locale=ms_MY");
+    }
+    public void soyung()
+    {
+        Application.OpenURL("http://sejongsc.co.kr/");
+    }
+    public void Pic()
+    {
+        Application.OpenURL("https://home.sejong.ac.kr/bbs/bbsview.do?bbsid=2049&pkid=16869&wslID=scc&currentPage=1&searchValue=&searchField=");
+    }
+    public void yasic()
+    {
+        Application.OpenURL("https://m.facebook.com/SJCESC/photos/a.312053695611127.1073741829.143385535811278/452719481544547/?locale=el_GR");
     }
 }
